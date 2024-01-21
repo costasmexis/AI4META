644b5456c7de14902a7ba545326886139526bc36
SpyrosAlvanakis <s.alvanakis@gmail.com>
2024-01-21 19:25:47 +0200

    init

--- machinelearning/__init__.py
+++ machinelearning/__init__.py
@@ -1,5 +1,6 @@
 from .mlestimator import MachineLearningEstimator
 from .mlpipeline import MLPipelines
+from .Features_explanation import Features_explanation
 # from .optuna_grid import optuna_grid
 
-__all__ = ['MLPipelines', 'MachineLearningEstimator']
+__all__ = ['MLPipelines', 'MachineLearningEstimator', 'Features_explanation']
