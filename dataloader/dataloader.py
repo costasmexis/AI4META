f0efb9e4d4e399d37f59864157db8394b43d5534
costasmexis <costasmexis@gmail.com>
2024-04-23 12:02:46 +0000

    upd

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -1,15 +1,24 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.feature_selection import SelectKBest, chi2, f_classif, mutual_info_classif, SelectPercentile
 from mrmr import mrmr_classif
+from sklearn.feature_selection import (
+    SelectKBest,
+    SelectPercentile,
+    chi2,
+    f_classif,
+    mutual_info_classif,
+)
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
+
 
 class DataLoader:
-    def __init__(self, label, csv_dir):
-        """Class to load a metabolomics dataset from a file.
-        Args:
-            label (str): name of the target column.
-            csv_dir (str): path to the csv file.
+    def __init__(self, label: str, csv_dir: str):
+        """
+        Constructor for the DataLoader class.
+
+        :param label: Target variable column name
+        :type label: str
+        :param csv_dir: Path to the csv file
+        :type csv_dir: str
         """
         self.csv_dir = csv_dir
         self.label = label
@@ -20,14 +29,17 @@ class DataLoader:
         self.selected_features = None
         self.supported_extensions = ['csv', 'tsv', 'txt']
         self.scaler = None 
-        self.__load_data() 
-        self.__encode_labels()
-    
-    def __load_data(self, index_col=0):
-        """Function to load data from a file.
-        Args:
-            index_col (int, optional): index column of dataset. Defaults to 0.
+        self._load_data() 
+        self._encode_labels()
+        
+    def _load_data(self, index_col=0):
         """
+        Function to load the dataset from the csv file.
+
+        :param index_col: Index column, defaults to 0
+        :type index_col: int, optional
+        """
+
         file_extension = self.csv_dir.split('.')[-1]
         if file_extension in self.supported_extensions:
             sep = ',' if file_extension == 'csv' else '\t'
@@ -38,65 +50,35 @@ class DataLoader:
         self.X = self.data.drop(self.label, axis=1)
         self.y = self.data[self.label].copy()
     
-    def __encode_labels(self):
-        """ Function to encode the target varable. From str to 1/0. """
+    def _encode_labels(self):
+        """
+        Function to encode the target labels.
+        """
+        
         label_encoder = LabelEncoder()
         self.y = label_encoder.fit_transform(self.y)
-        # self.label_mapping = {class_label: index for index, class_label in enumerate(label_encoder.classes_)}
-        # print("Label mapping:", self.label_mapping)
         self.label_mapping = {index: class_label for index, class_label in enumerate(label_encoder.classes_)}
         print("Label mapping:", self.label_mapping)
         
-    def missing_values(self, data=None, method='drop'):
-        ''' Function to handle missing values in the dataset.'''
-        initial_data=False
+    def missing_values(self, method='drop'):
+        """
+        Function to handle missing values in the dataset.
 
-        if data is None:
-            data = self.X
-            initial_data=True
-            total_missing = data.isnull().sum().sum()
-            print(f'Number of missing values: {total_missing}')
+        :param method: Method to be used, defaults to 'drop'
+        :type method: str, optional
+        """
+        total_missing = self.X.isnull().sum().sum()
+        print(f'Number of missing values: {total_missing}')
 
         if method == 'drop':
-            data.dropna(inplace=True)
+            self.X.dropna(inplace=True)
         elif method in ['mean', 'median', '0']:
-            if method == '0':
-                fill_value = 0  
-            else:
-                fill_value = getattr(data, method)()  
-            data.fillna(fill_value, inplace=True)
-        else:
-            raise Exception("Unsupported missing values method.")
-        
-        if initial_data:
-            self.X = data
-        else:
-            return data
+            fill_value = 0 if method == '0' else getattr(self.X, method)()
+            self.X.fillna(fill_value, inplace=True)
         
         
+    # TODO: FIX NORMALIZE FUNCTION
     def normalize(self, X=None, method='minmax', train_test_set=False, X_test=None):
-        """
-        Normalizes the dataset using specified method.
-
-        This method supports normalization using either MinMaxScaler or StandardScaler from the pandas library. 
-        If no dataset is explicitly passed to it, the method defaults to normalizing the instance's dataset.
-
-        Parameters:
-        - X (pandas.DataFrame, optional): The dataset to normalize. If not provided, the instance's dataset is used.
-        - method (str, optional): The normalization method to use. Defaults to 'minmax'. Options include:
-            - 'minmax': Uses MinMaxScaler for normalization.
-            - 'standard': Uses StandardScaler for normalization.
-
-        Returns:
-        - pandas.DataFrame: The normalized dataset. If `X` is not provided, the instance's dataset is updated in place and nothing is returned.
-
-        Raises:
-        - Exception: If an unsupported normalization method is specified.
-
-        Note:
-        - This method updates the instance's dataset if no `X` is provided. It also updates the instance's scaler attribute to the scaler used for normalization.
-        - A message indicating the completion of normalization is printed. If the instance's dataset is used, a message specifying the normalization method is also printed.
-        """
         initial_data=False
         
         if X is None:
@@ -124,62 +106,76 @@ class DataLoader:
             return X
     
     def feature_selection(self, X=None, y=None, method='mrmr', num_features=10, inner_method='chi2'):        
-        """ Function to perform Feature Selection.
-        Args:
-            method (str, optional): Method to use for feature selection. Defaults to 'mrmr'.
-            n_features (int, optional): Number of features to be selected. Defaults to 10.
-            inner_method (str, optional) ['chi2', 'f_classif', 'mutual_info_classif']: Inner method for SelectKBest. Defaults to 'chi2'.
-        """
-        if method not in ['mrmr', 'kbest', 'percentile']:
-            raise Exception("Unsupported feature selection method. Select one of 'mrmr', 'kbest', 'percentile'")
-        if inner_method not in ['chi2', 'f_classif', 'mutual_info_classif']:
-            raise Exception("Unsupported inner method. Select one of 'chi2', 'f_classif', 'mutual_info_classif'")
-        if method == 'percentile' and num_features is not None and (num_features >= 100 or num_features <= 0):
-            raise Exception("num_features for percentile option must be between 0 and 100.")
-        
-        datasetXy=False
-        if X is None and y is None:
-            X = self.X
-            y = self.y
-            datasetXy=True
+            """
+            Perform feature selection on the input dataset.
+
+            :param X: The input features, defaults to None
+            :type X: array-like, optional
+            :param y: The target variable, defaults to None
+            :type y: array-like, optional
+            :param method: The feature selection method to use, defaults to 'mrmr'
+            :type method: str, optional
+            :param num_features: The number of features to select, defaults to 10
+            :type num_features: int, optional
+            :param inner_method: The scoring method used for feature selection, defaults to 'chi2'
+            :type inner_method: str, optional
+            :return: The selected features or None if X and y are provided
+            :rtype: array-like or None
+            :raises Exception: If an unsupported feature selection method or inner method is selected
+            """
+
+            if method not in ['mrmr', 'kbest', 'percentile']:
+                raise Exception("Unsupported feature selection method. Select one of 'mrmr', 'kbest', 'percentile'")
+            if inner_method not in ['chi2', 'f_classif', 'mutual_info_classif']:
+                raise Exception("Unsupported inner method. Select one of 'chi2', 'f_classif', 'mutual_info_classif'")
+            if method == 'percentile' and num_features is not None and (num_features >= 100 or num_features <= 0):
+                raise Exception("num_features for percentile option must be between 0 and 100.")
+            
+            datasetXy=False
+            if X is None and y is None:
+                X = self.X
+                y = self.y
+                datasetXy=True
 
 
-        method_mapping = {
-            'chi2': chi2,
-            'f_classif': f_classif,
-            'mutual_info_classif': mutual_info_classif
-        }
-        scoring_function = method_mapping[inner_method]    
-        
-        if method == 'mrmr':
-            self.selected_features = mrmr_classif(X, y, K=num_features, show_progress=False)
-            X = X[self.selected_features]
+            method_mapping = {
+                'chi2': chi2,
+                'f_classif': f_classif,
+                'mutual_info_classif': mutual_info_classif
+            }
+            scoring_function = method_mapping[inner_method]    
+            
+            if method == 'mrmr':
+                self.selected_features = mrmr_classif(X, y, K=num_features, show_progress=False)
+                X = X[self.selected_features]
 
-        else:
-            if not isinstance(self.scaler, MinMaxScaler):
-                raise Exception("Feature selection method requires MinMaxScaler.")
-            if method == 'kbest':
-                X = SelectKBest(scoring_function, k=num_features).fit_transform(X, y)
+            else:
+                if not isinstance(self.scaler, MinMaxScaler):
+                    raise Exception("Feature selection method requires MinMaxScaler.")
+                if method == 'kbest':
+                    X = SelectKBest(scoring_function, k=num_features).fit_transform(X, y)
 
-            elif method == 'percentile':
-                X = SelectPercentile(scoring_function, percentile=num_features).fit_transform(X, y)
+                elif method == 'percentile':
+                    X = SelectPercentile(scoring_function, percentile=num_features).fit_transform(X, y)
+                else: 
+                    raise Exception("Unsupported feature selection method.")
+            
+            if datasetXy:
+                self.X = X
+                self.y = y
             else: 
-                raise Exception("Unsupported feature selection method.")
-        
-        if datasetXy:
-            self.X = X
-            self.y = y
-        else: 
-            return self.selected_features
+                return self.selected_features
         
 
     def create_test_data(self, output_dir='./test_data.csv'):
-        """ Function to generate a test data template csv file.
-        Args:
-            output_dir (str, optional): The directory to save the csv file. Defaults to './test_data.csv'.
+        """
+        Create a test dataset.
+
+        :param output_dir: The directory where the test data will be saved. Defaults to './test_data.csv'.
+        :type output_dir: str, optional
         """
         test_data = pd.DataFrame(columns=self.X.columns.values)
-        test_data.to_csv(output_dir)        
+        test_data.to_csv(output_dir)
         
     def __str__(self):
         ''' Function to print the dataset information.'''
