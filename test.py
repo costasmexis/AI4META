d3fb493065ea8cea71dbd29a2ecd3621646be75d
SpyrosAlvanakis <s.alvanakis@gmail.com>
2024-12-29 19:13:37 +0200

    icc run

--- test.py
+++ test.py
@@ -3,32 +3,24 @@ from machinelearning.mlexplain import MLExplainer
 from dataloader.eda import DataExplorer
 import pandas as pd
 
-datasets = ['epic_lc_ms_pos', 'epic_composite']#, 'chronic_fatigue','lung_cancer','periodontal_inflammation','epic_lc_ms_pos','epic_composite']
+datasets = ['ICC']
 
 for dataset in datasets:
     csv_dir = 'data/' + dataset + '.csv'
-    if dataset == 'epic_lc_ms_pos':
+    if (dataset == 'epic_lc_ms_pos') or(dataset == 'epic_lc_ms_neg'):
         label = 'group'
+    elif (dataset == 'epic_composite'):
+        label = 'Factor1'
+    elif (dataset == 'ICC'):
+        label = 'type'
+    elif (dataset == 'nhs_healthy') or (dataset == 'nhs_naive'):
+        label = 'label'
     else:
-        label= 'Factor_1' 
+        label = 'Class'
     
     print(f'STARTING WITH {dataset}')
-    mlpipe = MLPipelines(label=label, csv_dir=csv_dir)
+    mlpipe = MLPipelines(label=label, csv_dir=csv_dir, database_name='ai4meta.db')
     mlpipe.nested_cv(parallel='freely_parallel', info_to_db=True)
-    # mlpipe = MLPipelines(label=label, csv_dir=csv_dir)
-    # for estimator in estimators:
-    #     for evaluation_method in evaluation:
-    #         for inner in inner_selection:
-    #             mod, df = mlpipe.bayesian_search(estimator_name=estimator,scoring='matthews_corrcoef',boxplot=False, evaluation=evaluation_method, n_trials=100, cv=5, warnings_filter=True,training_method=inner,processors=4)
-    #             dataset_df_final = pd.concat([dataset_df_final,df], ignore_index=True)
-    #             dataset_df_final.to_csv('Final_Model_Results/' + dataset + '_final.csv')
-    #             print(f'FINISHED WITH {dataset} AND {estimator} AND {evaluation_method} AND {inner}')
-    
-#     eda = DataExplorer(label=label, csv_dir=csv_dir)
-#     feat = eda.statistical_difference(show_box=False)
-#     mlpipe.X = mlpipe.X[feat]
-#     for inner in inner_selection:
-#         df = mlpipe.nested_cv(parallel='freely_parallel',inner_selection=inner,name_add='StatDiffTrial')
-#         print(f'FINISHED WITH {dataset} AND {inner}')
-#     print(f'FINISHED WITH {dataset} COMPLETLY')
-# print('FINISHED ALL DATASETS')
+    mlpipe.rcv_accel(info_to_db=True)
+    print('Finished')
+
